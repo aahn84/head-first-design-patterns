2ec7c85b114dd61cf5dda06879fa9a1170cacdea
Angela Ahn <aahn@evo.com>
2021-02-12 00:38:14 -0800

    Singleton Pattern

--- HeadFirstDesignPatterns/Program.cs
+++ HeadFirstDesignPatterns/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using app.HeadFirstDesignPatterns.Decorator;
 using app.HeadFirstDesignPatterns.Observer;
+using app.HeadFirstDesignPatterns.Singleton;
 using HeadFirstDesignPatterns.Strategy;
-
 
 namespace app.HeadFirstDesignPatterns
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // Chapter 1 - Strategy
             Console.WriteLine("********** CH.1 - STRATEGY **********");
@@ -34,6 +34,20 @@ namespace app.HeadFirstDesignPatterns
             //Console.WriteLine("// IO //");
             //InputTest.Run();
             //Console.WriteLine();
+
+            //Chapter 4 - Factory
+            Console.WriteLine("********** CH.4 - FACTORY **********");
+            Console.WriteLine();
+            Console.WriteLine("// PIZZA TEST DRIVE //");
+            PizzaTestDrive.Run();
+            Console.WriteLine();
+
+            //Chapter 5 - Singleton
+            Console.WriteLine("********** CH.5 - SINGLETON **********");
+            Console.WriteLine();
+            Console.WriteLine("// CHOCOLATE FACTORY //");
+            ChocolateFactory.Run();
+            Console.WriteLine();
         }
     }
 }
