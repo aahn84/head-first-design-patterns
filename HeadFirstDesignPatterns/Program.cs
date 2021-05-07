b05328a1d74e150c5eb136a5a96c20848b6a9aa7
Angela Ahn <aahn@evo.com>
2021-05-06 23:21:42 -0700

    Part 1 State Pattern

--- HeadFirstDesignPatterns/Program.cs
+++ HeadFirstDesignPatterns/Program.cs
@@ -7,6 +7,7 @@ using app.HeadFirstDesignPatterns.IteratorAndCompositePattern;
 using app.HeadFirstDesignPatterns.IteratorAndCompositePattern.Composite;
 using app.HeadFirstDesignPatterns.ObserverPattern;
 using app.HeadFirstDesignPatterns.SingletonPattern;
+using app.HeadFirstDesignPatterns.StatePattern.GumballMachine;
 using app.HeadFirstDesignPatterns.TemplateMethodPattern.Barista;
 using app.HeadFirstDesignPatterns.TemplateMethodPattern.BaristaWithHook;
 using app.HeadFirstDesignPatterns.TemplateMethodPattern.DuckSort;
@@ -18,25 +19,25 @@ namespace app.HeadFirstDesignPatterns
     {
         private static void Main(string[] args)
         {
-            // Chapter 1 - Strategy
-            Console.WriteLine("********** CH.1 - STRATEGY **********");
-            Console.WriteLine("// MINI DUCK SIMULATOR //");
-            MiniDuckSimulator.Run();
-            Console.WriteLine();
+            //Chapter 1 - Strategy
+            //Console.WriteLine("********** CH.1 - STRATEGY **********");
+            //Console.WriteLine("// MINI DUCK SIMULATOR //");
+            //MiniDuckSimulator.Run();
+            //Console.WriteLine();
 
             //Chapter 2 - Observer
-            Console.WriteLine("********** CH.2 - OBSERVER **********");
-            Console.WriteLine();
-            Console.WriteLine("// WEATHER STATION //");
-            WeatherStation.Run();
-            Console.WriteLine();
+            //Console.WriteLine("********** CH.2 - OBSERVER **********");
+            //Console.WriteLine();
+            //Console.WriteLine("// WEATHER STATION //");
+            //WeatherStation.Run();
+            //Console.WriteLine();
 
             //Chapter 3 - Decorator
-            Console.WriteLine("********** CH.3 - DECORATOR **********");
-            Console.WriteLine();
-            Console.WriteLine("// STARBUZZ //");
-            StarbuzzCoffee.Run();
-            Console.WriteLine();
+            //Console.WriteLine("********** CH.3 - DECORATOR **********");
+            //Console.WriteLine();
+            //Console.WriteLine("// STARBUZZ //");
+            //StarbuzzCoffee.Run();
+            //Console.WriteLine();
 
             //Console.WriteLine();
             //Console.WriteLine("// IO //");
@@ -44,66 +45,76 @@ namespace app.HeadFirstDesignPatterns
             //Console.WriteLine();
 
             //Chapter 4 - Factory
-            Console.WriteLine("********** CH.4 - FACTORY **********");
-            Console.WriteLine();
-            Console.WriteLine("// PIZZA TEST DRIVE //");
-            PizzaTestDrive.Run();
-            Console.WriteLine();
+            //Console.WriteLine("********** CH.4 - FACTORY **********");
+            //Console.WriteLine();
+            //Console.WriteLine("// PIZZA TEST DRIVE //");
+            //PizzaTestDrive.Run();
+            //Console.WriteLine();
 
             //Chapter 5 - Singleton
-            Console.WriteLine("********** CH.5 - SINGLETON **********");
-            Console.WriteLine();
-            Console.WriteLine("// CHOCOLATE FACTORY //");
-            ChocolateFactory.Run();
-            Console.WriteLine();
+            //Console.WriteLine("********** CH.5 - SINGLETON **********");
+            //Console.WriteLine();
+            //Console.WriteLine("// CHOCOLATE FACTORY //");
+            //ChocolateFactory.Run();
+            //Console.WriteLine();
 
             //Chapter 6 - Command
-            Console.WriteLine("********** CH.6 - COMMAND **********");
-            Console.WriteLine();
-            Console.WriteLine("// SIMPLE REMOTE //");
-            SimpleRemoteControlTest.Run();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("// REMOTE LOADER //");
-            RemoteLoader.Run();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("// REMOTE LOADER WITH UNDO //");
-            RemoteLoaderWithUndo.Run();
-            Console.WriteLine();
+            //Console.WriteLine("********** CH.6 - COMMAND **********");
+            //Console.WriteLine();
+            //Console.WriteLine("// SIMPLE REMOTE //");
+            //SimpleRemoteControlTest.Run();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("// REMOTE LOADER //");
+            //RemoteLoader.Run();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("// REMOTE LOADER WITH UNDO //");
+            //RemoteLoaderWithUndo.Run();
+            //Console.WriteLine();
 
             //Chapter 7 - Adapter & Facade
-            Console.WriteLine("********** CH.7 - ADAPTER & FACADE **********");
-            Console.WriteLine();
-            Console.WriteLine("// DUCK ADAPTER //");
-            DuckAdapterTestDrive.Run();
-            Console.WriteLine();
+            //Console.WriteLine("********** CH.7 - ADAPTER & FACADE **********");
+            //Console.WriteLine();
+            //Console.WriteLine("// DUCK ADAPTER //");
+            //DuckAdapterTestDrive.Run();
+            //Console.WriteLine();
 
             //Chapter 8 - Template Method
-            Console.WriteLine("********** CH.8 - TEMPLATE METHOD **********");
-            Console.WriteLine();
-            Console.WriteLine("// BARISTA //");
-            BeverageTestDrive.Run();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("// BARISTA WITH HOOK //");
-            BeverageTestDriveWithHook.Run();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("// DUCK SORT //");
-            DuckSortTestDrive.Run();
-            Console.WriteLine();
+            //Console.WriteLine("********** CH.8 - TEMPLATE METHOD **********");
+            //Console.WriteLine();
+            //Console.WriteLine("// BARISTA //");
+            //BeverageTestDrive.Run();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("// BARISTA WITH HOOK //");
+            //BeverageTestDriveWithHook.Run();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("// DUCK SORT //");
+            //DuckSortTestDrive.Run();
+            //Console.WriteLine();
 
             //Chapter 9 - Iterator & Composite
-            Console.WriteLine("********** CH.9 - ITERATOR & COMPOSITE **********");
+            //Console.WriteLine("********** CH.9 - ITERATOR & COMPOSITE **********");
+            //Console.WriteLine();
+            //Console.WriteLine("*** ITERATOR ***");
+            //Console.WriteLine();
+            //Console.WriteLine("// DINER MENU //");
+            //DinerMenuTestDrive.Run();
+            //Console.WriteLine();
+            //Console.WriteLine("*** COMPOSITE ***");
+            //MenuTestDrive.Run();
+            //Console.WriteLine();
+
+            //Chapter 10 - State
+            Console.WriteLine("********** CH.10 - STATE **********");
             Console.WriteLine();
-            Console.WriteLine("*** ITERATOR ***");
+            Console.WriteLine("// GUMBALL MACHINE //");
+            GumballMachineTestDrive.Run();
             Console.WriteLine();
-            Console.WriteLine("// DINER MENU //");
-            DinerMenuTestDrive.Run();
-            Console.WriteLine();
-            Console.WriteLine("*** COMPOSITE ***");
-            MenuTestDrive.Run();
+            Console.WriteLine("// GUMBALL MACHINE WITH STATE //");
+            //MenuTestDrive.Run();
             Console.WriteLine();
         }
     }
---
+++ HeadFirstDesignPatterns/StatePattern/GumballMachine/GumballMachineTestDrive.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using app.HeadFirstDesignPatterns.IteratorAndCompositePattern;
+
+namespace app.HeadFirstDesignPatterns.StatePattern.GumballMachine
+{
+    public static class GumballMachineTestDrive
+    {
+        public static void Run()
+        {
+            GumballMachine gumballMachine = new GumballMachine(5);
+
+            Console.WriteLine(gumballMachine);
+
+            gumballMachine.InsertQuarter();
+            gumballMachine.TurnCrank();
+
+            Console.WriteLine(gumballMachine);
+
+            gumballMachine.InsertQuarter();
+            gumballMachine.EjectQuarter();
+            gumballMachine.TurnCrank();
+
+            Console.WriteLine(gumballMachine);
+
+            gumballMachine.InsertQuarter();
+            gumballMachine.TurnCrank();
+            gumballMachine.InsertQuarter();
+            gumballMachine.TurnCrank();
+            gumballMachine.EjectQuarter();
+
+            Console.WriteLine(gumballMachine);
+
+            gumballMachine.InsertQuarter();
+            gumballMachine.InsertQuarter();
+            gumballMachine.TurnCrank();
+            gumballMachine.InsertQuarter();
+            gumballMachine.TurnCrank();
+            gumballMachine.InsertQuarter();
+            gumballMachine.TurnCrank();
+
+            Console.WriteLine(gumballMachine);
+        }
+    }
+}
